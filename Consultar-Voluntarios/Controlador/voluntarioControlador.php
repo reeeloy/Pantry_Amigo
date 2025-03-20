85826f239d931e29a648ceacd650a517287f1e1e
Daniel cardenas <df028231@gmail.com>
2025-03-20 00:37:31 -0500

    Agrego la consulta de voluntarios falta terminar la interfaz

---
+++ Consultar-Voluntarios/Controlador/voluntarioControlador.php
@@ -0,0 +1,23 @@
+<?php
+require_once "../Modelo/voluntarioModelo.php";
+
+class VoluntarioControlador {
+    private $modelo;
+
+    public function __construct() {
+        $this->modelo = new Voluntario();
+    }
+
+    public function buscar() {
+        if (isset($_POST['cedula'])) {
+            $cedula = $_POST['cedula'];
+            $voluntario = $this->modelo->buscarVoluntario($cedula);
+            return $voluntario;
+        }
+        return null;
+    }
+}
+
+$controlador = new VoluntarioControlador();
+$voluntario = $controlador->buscar();
+?>
---
+++ Consultar-Voluntarios/Modelo/voluntarioModelo.php
@@ -0,0 +1,18 @@
+<?php
+require_once "../Controlador/conexionBD.php";
+
+class Voluntario{
+    private $conexion;
+
+    public function __construct() {
+        $this->conexion = (new conexionBD())->conexion;
+    }
+
+    public function buscarVoluntario($cedula) {
+        $sql = "SELECT * FROM tbl_voluntarios WHERE Vol_Cedula = ?";
+        $stmt = $this->conexion->prepare($sql);
+        $stmt->execute([$cedula]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
+?>
