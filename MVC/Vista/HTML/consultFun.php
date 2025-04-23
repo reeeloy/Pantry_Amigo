7761c67f4a57b699b40086ab71e9fea38ee14e5e
Isabela Jimnez Tovar <isabela@gmail.com>
2025-04-23 05:36:43 -0500

    cammmmbioos

---
+++ MVC/Vista/HTML/consultFun.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html> 
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Consulta de Fundaciones</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" crossorigin="anonymous" />
+    <link rel="stylesheet" href="estiloFundaciones.css">
+</head>
+
+<body>
+    <header>
+        <div class="logito">
+            <img src="../IMG/Logo.png" alt="Pantry Amigo Logo">
+            <span>Pantry Amigo</span>
+        </div>
+        <div class="navbar"> 
+            <nav>
+                <a href="index.php" class="home-button">INICIO</a>
+                <input type="search" placeholder="Buscar" class="search-bar">
+                <a href="" class="help-icon">
+                    <img src="IMG/Ayuda.png" alt="Ayuda">
+                </a>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+           
+            <div class="fundacion-card">
+                <img src="donacion.jpg" alt="Imagen Fundación">
+                <div class="fundacion-info">
+                    <h4>FUNDACION/XXXXX</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+                    <a href=".../CSS/revisarFundacion.php" class="ver-mas-btn">VER MÁS</a>
+                </div>
+            </div>
+
+            <div class="fundacion-card">
+                <img src="donacion.jpg" alt="Imagen Fundación">
+                <div class="fundacion-info">
+                    <h4>FUNDACION/YYYYY</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+                    <a href=".../CSS/revisarFundacion.php" class="ver-mas-btn">VER MÁS</a>
+                </div>
+            </div>
+
+            <div class="fundacion-card">
+                <img src="donacion.jpg" alt="Imagen Fundación">
+                <div class="fundacion-info">
+                    <h4>FUNDACION/YYYYY</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+                    <a href=".../CSS/revisarFundacion.php" class="ver-mas-btn">VER MÁS</a>
+                </div>
+            </div>
+
+            <div class="fundacion-card">
+                <img src="donacion.jpg" alt="Imagen Fundación">
+                <div class="fundacion-info">
+                    <h4>FUNDACION/YYYYY</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+                    <a href=".../CSS/revisarFundacion.php" class="ver-mas-btn">VER MÁS</a>
+                </div>
+            </div>
+
+            <div class="fundacion-card">
+                <img src="donacion.jpg" alt="Imagen Fundación">
+                <div class="fundacion-info">
+                    <h4>FUNDACION/YYYYY</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+                    <a href=".../CSS/revisarFundacion.php" class="ver-mas-btn">VER MÁS</a>
+                </div>
+            </div>
+            
+        </div>
+    </main>
+
+    <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-logo">
+                <h2>Pantry Amigo</h2>
+                <p>Conectando ayuda con quienes más la necesitan.</p>
+            </div>
+            <div class="footer-links">
+                <h3>Enlaces</h3>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="ConsultarCasos.php">Casos de Donación</a></li>
+                    <li><a href="ConsultarFundacion.php">Fundaciones</a></li>
+                    <li><a href="ConsultarVoluntario.php">Voluntariados</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </div>
+            <div class="footer-social">
+                <h3>Síguenos</h3>
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>&copy; 2025 Pantry Amigo - Todos los derechos reservados.</p>
+        </div>
+    </footer>
+</body>
+</html>
+
+                        
+      
+   
