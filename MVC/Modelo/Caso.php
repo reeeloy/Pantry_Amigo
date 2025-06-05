2168797429881efd1e0939867e784190c1446622
Laura <bolivarmelolaura@gmail.com>
2025-06-05 15:23:26 -0500

    solo casos dinero

--- MVC/Modelo/Caso.php
+++ MVC/Modelo/Caso.php
@@ -32,50 +32,12 @@ class Caso {
         }
         return false;
     }
-    
-
-    //Registrar Caso Recurso
-    public function registrarCasoRecursos($nombre, $descripcion, $fechaInicio, $fechaFin, $estado, $puntoRec, $imagen, $voluntariado, $fundacionId, $categoriaNombre) {
-        if ($this->conexion->abrir()) {
-            // Si no se proporciona una imagen, se guarda como NULL en la base de datos
-            $imagenSQL = $imagen ? "'$imagen'" : "NULL";
-    
-            $sql = "INSERT INTO tbl_casos_recursos (
-                        Caso_Nombre, Caso_Descripcion, Caso_Fecha_Inicio, Caso_Fecha_Fin, Caso_Estado, Caso_Punto_Recoleccion, Caso_Imagen, 
-                        Caso_Voluntariado, Caso_Fund_Id, Caso_Cat_Nombre
-                    ) 
-                    VALUES (
-                        '$nombre', '$descripcion','$fechaInicio', '$fechaFin', '$estado', '$puntoRec', $imagenSQL, 
-                        '$voluntariado', '$fundacionId', '$categoriaNombre'
-                    )";
-    
-            $this->conexion->consulta($sql);
-            $filasAfectadas = $this->conexion->obtenerFilasAfectadas();
-            $this->conexion->cerrar();
-            return $filasAfectadas > 0;
-        }
-        return false;
-    }
-
-   
 
     // 🔍 Obtener todos los casos activos dinero
     public function obtenerCasosActivosDinero() {
         if ($this->conexion->abrir()) {
             
             $sql = "SELECT Caso_Id, Caso_Nombre, Caso_Descripcion FROM tbl_casos_dinero WHERE Caso_Estado = 'Activo'";
-            $resultado = $this->conexion->consulta($sql);
-            $this->conexion->cerrar();
-            return $resultado;
-        }
-        return null;
-    }
-
-    // 🔍 Obtener todos los casos activos dinero
-    public function obtenerCasosActivosRecursos() {
-        if ($this->conexion->abrir()) {
-            
-            $sql = "SELECT Caso_Id, Caso_Nombre, Caso_Descripcion FROM tbl_casos_recursos WHERE Caso_Estado = 'Activo'";
             $resultado = $this->conexion->consulta($sql);
             $this->conexion->cerrar();
             return $resultado;
